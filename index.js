8a80528c894896d3691974fd0869dffd2b624959
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-02-16 18:26:36 +0300

    создал базу в MongoDB и подключился к ней с помощью mongoose

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
+mongoose
+  .connect(
+    "mongodb+srv://pioneerbeat:Andrey@fs.h3wezc2.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("DB ok"))
+  .catch((err) => console.log("DB error", err));
 const app = express();
 
 app.use(express.json());
@@ -9,18 +16,20 @@ app.get("/", (req, res) => {
   res.send("hello world");
 });
 
-
 app.post("/auth/login", (req, res) => {
   console.log(req.body);
-  const token = jwt.sign({
-    email:req.body.email,
-    fullName: "Андрей Чебанов",
-}, "password")
-    res.json({
-        success: true,
-        token
-    })
-})
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+      fullName: "Андрей Чебанов",
+    },
+    "password"
+  );
+  res.json({
+    success: true,
+    token,
+  });
+});
 
 app.listen(4444, (err) => {
   if (err) {
