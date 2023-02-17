9261e695516e0619ba63a2cafe9214720836a6f5
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-02-17 13:56:58 +0300

    подключил и настроил валидацию полей, запросы проверяю через insomnia

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
-import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
+import { validationResult } from "express-validator";
+import { registerValidation } from "./validations/auth.js";
 
 mongoose
   .connect(
@@ -12,23 +13,14 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
-
-app.post("/auth/login", (req, res) => {
-  console.log(req.body);
-  const token = jwt.sign(
-    {
-      email: req.body.email,
-      fullName: "Андрей Чебанов",
-    },
-    "password"
-  );
+app.post("/auth/register", registerValidation, (req, res) => {
+  const error = validationResult(req);
+  if(!error.isEmpty()){
+    return res.status(400).json(error.array())
+  }
   res.json({
     success: true,
-    token,
-  });
+  })
 });
 
 app.listen(4444, (err) => {
