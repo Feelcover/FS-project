ce8603a7baf9f521277fcdcf9adb08fe7245757c
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-02-17 16:32:17 +0300

    рефакторинг, вынес функции в другой файл

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
-import { validationResult } from "express-validator";
 import { registerValidation } from "./validations/auth.js";
-import UserSchema from "./models/User.js";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
 import authMiddleware from "./utils/authMiddleware.js";
+import { login, me, register } from "./controllers/UserController.js";
 
 mongoose
   .connect(
@@ -17,109 +14,12 @@ const app = express();
 
 app.use(express.json());
 
-//Получение запроса авторизации
-app.post("/auth/login", async (req, res) => {
-  try {
-    const user = await UserSchema.findOne({ email: req.body.email });
-
-    if (!user) {
-      return req.status(404).json({
-        message: "Неверный логин или пароль",
-      });
-    }
-    const password = await bcrypt.compare(
-      req.body.password,
-      user._doc.passwordHash
-    );
-    if (!password) {
-      return req.status(404).json({
-        message: "Неверный логин или пароль",
-      });
-    }
-    const { passwordHash, ...UserData } = user._doc;
-
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      "secret",
-      {
-        expiresIn: "30d",
-      }
-    );
-
-    res.json({
-      ...UserData,
-      token,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      message: "Не удалось провести авторизацию",
-    });
-  }
-});
-
-//Получение запроса регистрации
-app.post("/auth/register", registerValidation, async (req, res) => {
-  try {
-    const error = validationResult(req);
-    if (!error.isEmpty()) {
-      return res.status(400).json(error.array());
-    }
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-
-    const doc = new UserSchema({
-      email: req.body.email,
-      passwordHash: hash,
-      fullName: req.body.fullName,
-      avatarUrl: req.body.avatarUrl,
-    });
-
-    const user = await doc.save();
-    const { passwordHash, ...UserData } = user._doc;
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      "secret",
-      {
-        expiresIn: "30d",
-      }
-    );
-
-    res.json({
-      ...UserData,
-      token,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      message: "Не удалось провести регистрацию",
-    });
-  }
-});
-
-app.get("/auth/me", authMiddleware, async (req, res) => {
-  try {
-    const user = await UserSchema.findById(req.userId);
-
-    if (!user) {
-      return res.json({ message: "Пользователь не найден" });
-    }
-
-    const { passwordHash, ...UserData } = user._doc;
-
-    res.json(UserData);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      message: "Пользователь не найден",
-    });
-  }
-});
+//Обработка запроса авторизации
+app.post("/auth/login", login);
+//Обработка запроса регистрации
+app.post("/auth/register", registerValidation, register);
+//Обработка запроса данных пользователя
+app.get("/auth/me", authMiddleware, me);
 
 app.listen(4444, (err) => {
   if (err) {
