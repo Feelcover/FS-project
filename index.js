353b4cdc3803b94432d5740ecdb3a5c34113fcae
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-02-17 15:09:34 +0300

    зашифровал пароль с помощью bcrypt, в пост запросе возвращаю токен, так же обернул всё в try catch

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import { validationResult } from "express-validator";
 import { registerValidation } from "./validations/auth.js";
+import UserSchema from "./models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
 
 mongoose
   .connect(
-    "mongodb+srv://pioneerbeat:Andrey@fs.h3wezc2.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://pioneerbeat:Andrey@fs.h3wezc2.mongodb.net/blog?retryWrites=true&w=majority"
   )
   .then(() => console.log("DB ok"))
   .catch((err) => console.log("DB error", err));
@@ -13,14 +16,43 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/auth/register", registerValidation, (req, res) => {
-  const error = validationResult(req);
-  if(!error.isEmpty()){
-    return res.status(400).json(error.array())
+app.post("/auth/register", registerValidation, async (req, res) => {
+  try {
+    const error = validationResult(req);
+    if (!error.isEmpty()) {
+      return res.status(400).json(error.array());
+    }
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const doc = new UserSchema({
+      email: req.body.email,
+      passwordHash: hash,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+    });
+
+    const user = await doc.save();
+    const {passwordHash, ...UserData} = user._doc
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      }, "secret",
+      {
+        expiresIn:"30d",
+      }
+    )
+
+    res.json({
+      ...UserData, token
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось провести регистрацию",
+    });
   }
-  res.json({
-    success: true,
-  })
 });
 
 app.listen(4444, (err) => {
